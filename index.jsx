ccfdb701024c30d9be9aa9ad83929b1e8061bbb6
Alex Chuchunkov <scarywound@gmail.com>
2016-03-20 02:49:34 +0300

    components are not pure js; added support for react-devtools

--- index.jsx
+++ index.jsx
@@ -1,8 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-import {Root} from './src/js/components/Root';
+import {Root} from './src/components/Root';
 
 ReactDOM.render(
     <Root/>,
     document.getElementById('root'));
+
+if (typeof window !== 'undefined') {
+    window.React = React;
+}
