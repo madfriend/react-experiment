d1d1bcd42f95bb24aae2b0d7d00a652ae9f9dcbf
Alex Chuchunkov <scarywound@gmail.com>
2016-03-29 01:02:56 +0300

    more tweaks, got the correct folder list

--- api/server.js
+++ api/server.js
@@ -1,15 +1,20 @@
-var fs = require('fs');
-var express = require('express');
-var dummyjson = require('dummy-json');
+var fs = require("fs");
+var express = require("express");
+var dummyjson = require("dummy-json");
 
-var template = fs.readFileSync('api-template.hbs', {encoding: 'utf8'});
+var template = fs.readFileSync("api-template.hbs", {encoding: "utf8"});
 var app = express();
 
-app.get('/messages', function(req, res) {
+app.get("/messages", function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.set('Content-Type', 'application/json');
-  res.send(dummyjson.parse(template));
+  res.set("Content-Type", "application/json");
+
+  var myMockdata = {
+    folders: ["Inbox", "Important", "Sent", "Spam"]
+  };
+
+  res.send(dummyjson.parse(template, {mockdata: myMockdata}));
 });
 
 app.listen(8080);
